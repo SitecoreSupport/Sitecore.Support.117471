deb07a034ea11dd85fc7e824e74ce797437baa4a
DK\psk <psk@sitecore.net>
2018-12-15 14:28:02 +0200

    Preparation

---
+++ src/Sitecore.Support.117471/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.117471")]
+[assembly: AssemblyProduct("Sitecore.Support.117471")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Rework the RefreshTree command from the Developer Tab")]
+
